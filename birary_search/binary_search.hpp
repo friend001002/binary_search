cbf2f54a9e13ac9ca1349f99891a8771cb463e71
Anton Merzlyakov <friend001002@live.ru>
2019-07-21 11:45:14 +0300

    Add exception version

--- birary_search/binary_search.hpp
+++ birary_search/binary_search.hpp
@@ -9,6 +9,30 @@
 
 #include <cmath>
 
+class Binary_search_ex : public std::runtime_error
+{
+  public:
+
+  explicit Binary_search_ex(const char *msg) : runtime_error(msg)
+  {
+    
+  }
+
+  explicit Binary_search_ex(std::string& msg) : runtime_error(msg)
+  {
+
+  }
+
+  virtual ~Binary_search_ex() noexcept
+  {
+  }
+
+  virtual const char* what() const noexcept
+  {
+    return std::runtime_error::what();
+  }
+};
+
 template<class T>
 class Binary_search
 {
@@ -22,15 +46,19 @@ class Binary_search
 
   std::vector<T> Get_data() const;
 
+  size_t Get_iterations() const;
+
   void Set_data(const std::vector<T>& data);
 
   virtual ~Binary_search();
 
-  bool Find(T element, std::size_t& result) const;
+  bool   Find(T element, std::size_t& result);
+  size_t Find(T to_find) noexcept(false);
 
   private:
 
   std::vector<T> data_;
+  size_t iterations_;
 };
 
 #endif // _BINARY_SEARCH_HPP_
